3ce8f07e69e0f6ec2db2100f5a5255b35759f96f
0xmohitsen <mohitsen0102@gmail.com>
2025-05-17 22:51:21 +0530

    Fixed logout + added userimage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { auth, signIn, signOut } from "@/app/auth";
-import GitHub from "next-auth/providers/github";
 
 const Navbar = async () => {
   const session = await auth();
+  console.log("What is inside in session :", session);
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -20,16 +20,26 @@ const Navbar = async () => {
               </Link>
 
               <form
-                onClick={async () => {
+                action={async () => {
                   "use server";
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">Logout</button>
+                <button type="submit" className="cursor-pointer">
+                  Logout
+                </button>
               </form>
 
-              <Link href={`/user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>
+                  <Image
+                    src={session?.user?.image ?? "/logo.png"}
+                    alt="user image"
+                    width={40}
+                    height={40}
+                    className="rounded-full border"
+                  />
+                </span>
               </Link>
             </>
           ) : (
@@ -39,7 +49,9 @@ const Navbar = async () => {
                 await signIn("github");
               }}
             >
-              <button type="submit">Login</button>
+              <button type="submit" className="cursor-pointer">
+                Login
+              </button>
             </form>
           )}
         </div>
